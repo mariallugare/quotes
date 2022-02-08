42a1b878ca58e34950bbadc43999c966fe6f46f7
mariallugare <mariallugare@gmail.com>
2022-02-08 11:37:03 -0100

    create a description component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,9 +8,5 @@ import { Quotes } from './quotes';
 export class AppComponent {
   //  title = 'quotes-app';
 
-  quotes:Quotes[] =[
-    {id:1,quote:'life is short',author:'marial',name:'Alphine',upvotes:0,downvotes:0 },
-    {id:2,quote:'we will win',author:'marial',name:'Alphine',upvotes:0,downvotes:0 },
-    {id:3,quote:'i love ',author:'marial',name:'Alphine',upvotes:0,downvotes:0 }
-  ]
+ 
 }
--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -1,12 +1,16 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Quotes } from '../quotes';
 @Component({
   selector: 'app-quote',
   templateUrl: './quote.component.html',
   styleUrls: ['./quote.component.css']
 })
 export class QuoteComponent implements OnInit {
-
+  quotes:Quotes[] =[
+    {id:1,quote:'life is short',author:'marial',name:'Alphine',upvotes:0,downvotes:0 },
+    {id:2,quote:'we will win',author:'marial',name:'Alphine',upvotes:0,downvotes:0 },
+    {id:3,quote:'i love ',author:'marial',name:'Alphine',upvotes:0,downvotes:0 }
+  ]
   constructor() { }
 
   ngOnInit(): void {
