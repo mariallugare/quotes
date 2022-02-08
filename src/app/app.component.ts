552d65638276f4edba1d553e08fc54dd8288f186
mariallugare <mariallugare@gmail.com>
2022-02-08 11:24:25 -0100

    create a quote component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,6 +9,8 @@ export class AppComponent {
   //  title = 'quotes-app';
 
   quotes:Quotes[] =[
-    {id:1,quote:'life is short',author:'marial',name:'Alphine',upvotes:0,downvotes:0 }
+    {id:1,quote:'life is short',author:'marial',name:'Alphine',upvotes:0,downvotes:0 },
+    {id:2,quote:'we will win',author:'marial',name:'Alphine',upvotes:0,downvotes:0 },
+    {id:3,quote:'i love ',author:'marial',name:'Alphine',upvotes:0,downvotes:0 }
   ]
 }
